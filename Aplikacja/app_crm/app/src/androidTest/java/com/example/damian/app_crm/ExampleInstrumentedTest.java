ec4c83bdf440e129222c3ff2fcd159202cc86984
Dawid Wisniewski <42553780+DawidVVisniewski@users.noreply.github.com>
2019-04-05 10:14:44 +0100

    Update ExampleInstrumentedTest.java

--- Aplikacja/app_crm/app/src/androidTest/java/com/example/damian/app_crm/ExampleInstrumentedTest.java
+++ Aplikacja/app_crm/app/src/androidTest/java/com/example/damian/app_crm/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.damian.app_crm;
+package com.example.dawid.app_crm;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.damian.app_crm", appContext.getPackageName());
+        assertEquals("com.example.dawid.app_crm", appContext.getPackageName());
     }
 }
